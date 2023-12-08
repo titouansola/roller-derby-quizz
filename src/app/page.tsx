9f4ed5629b6c737230e18e85ae816d409baecd20
Titouan Sola <titouansola0@gmail.com>
2023-12-08 18:55:46 +0100

    revalidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import QuizzRunner from '@internals/features/quizz/components/quizz-runner.component';
 
+export const revalidate = 5;
+
 export default function Home() {
   return <QuizzRunner />;
 }
